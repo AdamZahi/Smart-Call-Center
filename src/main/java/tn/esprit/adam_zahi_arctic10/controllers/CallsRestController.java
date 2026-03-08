e9e31c8c58c3d361d0dec67ba9821ffffceec3ba
Adam zahi <zahi.adem10@gmail.com>
2026-03-08 18:21:11 +0100

    Add AI system assignment and auto-assignment features to calls

--- src/main/java/tn/esprit/adam_zahi_arctic10/controllers/CallsRestController.java
+++ src/main/java/tn/esprit/adam_zahi_arctic10/controllers/CallsRestController.java
@@ -6,51 +6,61 @@ import tn.esprit.adam_zahi_arctic10.entities.Calls;
 import tn.esprit.adam_zahi_arctic10.services.ICallsServices;
 
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("calls")
-// http:://localhost:8082/api/calls
 public class CallsRestController {
     private final ICallsServices callsServices;
-
-    // http:://localhost:8082/api/calls/add
     @PostMapping("add")
     public Calls addCalls(@RequestBody Calls call) {
         return callsServices.addCall(call);
     }
-
-    // http:://localhost:8082/api/calls/update
     @PutMapping("update")
     public Calls updateCalls(@RequestBody Calls call) {
         return callsServices.updateCall(call);
     }
-
-    // http:://localhost:8082/api/calls/delete/{id}
     @DeleteMapping("delete/{id}")
     public void deleteCallsById(@PathVariable long id) {
         callsServices.deleteById(id);
     }
-
-    // http:://localhost:8082/api/calls/get/{id}
     @GetMapping("get/{id}")
     public Calls getById(@PathVariable long id) {
         return callsServices.getById(id);
     }
-
-    // http:://localhost:8082/api/calls/get
     @GetMapping("get")
     public List<Calls> getAllCalls() {
         return callsServices.getAll();
     }
-
     @PutMapping("assignToAgent/{callId}/{agentId}")
     public Calls assignedToAgent(@PathVariable long callId, @PathVariable long agentId) {
         return callsServices.assignedToAgent(callId, agentId);
     }
-
     @PostMapping("addAndAssignToAgent/{agentId}")
     Calls assignedToAgent (@RequestBody Calls call, @PathVariable long agentId){
         return callsServices.assignedToAgent(call, agentId);
     }
+
+    @PutMapping("assignToAISystem/{callId}/{aiSystemId}")
+    public Calls assignedToAISystem(@PathVariable long callId, @PathVariable long aiSystemId) {
+        return callsServices.assignedToAISystem(callId, aiSystemId);
+    }
+
+    @GetMapping("callRequiresHumanAgent/{callId}")
+    public boolean callRequiresHumanAgent(@PathVariable long callId) {
+        Calls call = callsServices.getById(callId);
+        return callsServices.callRequiresHumanAgent(call);
+    }
+
+    @PutMapping("autoAssign")
+    public void autoAssignCallsToAgents(@RequestBody Set<Long> callIds) {
+        callsServices.autoAssignCallsToAgents(callIds);
+    }
+
+    @PutMapping("assignCallsToAgents")
+    public void assignCallsToAgents(@RequestBody Set<Long> callIds) {
+        callsServices.assignCallsToAgents(callIds);
+    }
+
 }
