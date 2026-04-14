a1181a2ee4f4e6a8dab7a3760be6ce06f9b5a6af
Adam zahi <zahi.adem10@gmail.com>
2026-04-14 10:27:34 +0100

    Enable Spring scheduling

--- src/main/java/tn/esprit/adam_zahi_arctic10/AdamZahiArcTic10Application.java
+++ src/main/java/tn/esprit/adam_zahi_arctic10/AdamZahiArcTic10Application.java
@@ -2,7 +2,9 @@ package tn.esprit.adam_zahi_arctic10;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class AdamZahiArcTic10Application {
 
